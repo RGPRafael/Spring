90ac39a1c7b671d87a78ebe7fdedb7ad18643f5d
rafael <rafael@Lulinha>
2023-12-01 17:06:15 -0300

    pequenas modificações para receber dados do front end, projeto tem que ser o mais simples possivel

--- src/main/java/com/CRUD/CRUD/Controller/HunterController.java
+++ src/main/java/com/CRUD/CRUD/Controller/HunterController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +41,7 @@ define a controller and to
 indicate that the return value of the 
 methods should be be bound to the web response body.
 */
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/hunters")
 public class HunterController {
